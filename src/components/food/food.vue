e32f2fa32bf0f248ef7483592778eedaa8794766
guakun <2306831967@qq.com>
2018-06-04 11:12:26 +0800

    components food some dom

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -1,6 +1,6 @@
 <template>
   <transition name="move">
-    <div class="food" v-show="showFlag">
+    <div class="food" ref="food" v-show="showFlag">
       <div class="food-content">
         <div class="image-header">
           <img :src="food.image">
@@ -8,12 +8,30 @@
             <i class="icon-arrow_lift"></i>
           </div>
         </div>
+        <div class="content">
+          <h1 class="title">{{ food.name }}</h1>
+          <div class="detail">
+            <span class="sell-count">月售{{ food.sellCount }}</span>
+            <span class="rating">好评率{{ food.rating }}%</span>
+          </div>
+          <div class="price">
+            <span class="now">￥{{ food.price }}</span>
+            <span class="old" v-show="food.oldPrice">￥{{ food.oldPrice }}</span>
+          </div>
+        </div>
+        <div class="cartcontrol-wrapper">
+          <cartcontrol :food="food"></cartcontrol>
+        </div>
       </div>
     </div>
   </transition>
 </template>
 
 <script>
+import BScroll from 'better-scroll'
+
+import cartcontrol from 'components/cartcontrol/cartcontrol'
+
 export default {
   props: {
     food: {
@@ -29,12 +47,24 @@ export default {
   methods: {
     show() {
       this.showFlag = true
+      this.$nextTick(() => {
+        if(!this.scroll) {
+          this.scroll = new BScroll(this.$refs.food, {
+            click: true
+          })
+        } else {
+          this.scroll.refresh()
+        }
+      })
     },
     hide() {
       this.showFlag = false
     }
   },
-  computed: {}
+  computed: {},
+  components: {
+    cartcontrol 
+  }
 }
 </script>
 
@@ -72,4 +102,33 @@ export default {
         padding 10px
         font-style 20px
         color #fff
+  .content
+    padding 18px
+    .title
+      line-height 14px
+      margin-bottom 8px
+      font-size 14px
+      font-weight 700
+      color rgb(7, 17, 27)
+    .detail
+      margin-bottom 18px
+      line-height 10px
+      font-size 0
+      height 10px
+      .sell-count, .rating
+        font-size 10px
+        color rgb(147, 153, 159)
+      .sell-count
+        margin-right 12px
+    .price
+      font-weight 700
+      line-height 24px
+      .now
+        margin-right 8px
+        font-size 14px
+        color rgb(240, 20, 20)
+      .old
+        text-decoration line-through
+        font-size 10px
+        color rgb(147, 153, 159)
  </style>
